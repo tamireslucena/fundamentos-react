468e827b32fc6d4918bcd8f3198236b4d66f0799
Tamires Lucena <tamiresb.lucena@gmail.com>
2023-08-05 19:14:21 -0300

    feat: add clock container

---
+++ src/containers/Home/Clock/index.tsx
@@ -0,0 +1,33 @@
+// library methods
+import { useEffect, useState } from "react";
+
+// utilities
+import { formatTime } from "../../../utils/dateHelper";
+
+// styles
+import "./index.css"
+
+function Clock() {
+  const [clock, setClock] = useState(new Date());
+
+  const updateClock = () => { 
+    setClock(new Date());
+  };
+
+  useEffect(() => {
+    // update the clock state every 1s
+    const interval = setInterval(updateClock, 1000);
+
+    return () => {
+      clearInterval(interval)
+    };
+  }, []);
+
+  return (
+    <div className="Clock">
+      {formatTime(clock)}
+    </div>
+  );
+}
+
+export default Clock;
